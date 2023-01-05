3537687ba149e0b1470209d1750c40c09b63997f
SteveAmCoden <smkhitar@smail.uni-koeln.de>
2023-01-05 18:21:37 +0100

    Added example program

--- Main.java
+++ Main.java
@@ -3,5 +3,24 @@ public class Main {
     public static void main(String [] args){
         System.out.println("Code at line 4");
         System.out.println("Code at line 5 are also added in the main branch!");
+
+        // Example Program
+
+        int[] anArray;	        // DECLARE an array of integers
+
+        anArray = new int[10];	// CREATE an array of integers
+
+        // assign a value to each array element
+        for (int i = 0; i < anArray.length; i++) {
+            anArray[i] = i;
+        }
+
+        // print a value from each array element
+        for (int i = 0; i < anArray.length; i++) {
+            System.out.print(anArray[i] + " ");
+        }
+        System.out.println();
     }
+
+}
 }
